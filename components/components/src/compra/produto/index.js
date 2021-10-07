e60cff15ed0f1636ce15d4f5efdecd9e723313d4
heloysa <santos.heloysaarruda@gmail.com>
2021-10-07 18:21:25 -0300

    falta index produto e detalhe

--- components/components/src/compra/produto/index.js
+++ components/components/src/compra/produto/index.js
@@ -1,8 +1,13 @@
+import {Link} from 'react-router-dom'
 import Container from './styled'
 
 
 export default function Pr(){
     return(
-        <Container></Container>
+        <Container>
+            <img className="capa"src={props.info.imagem} alt=""/>
+            <div className="titulo">{props.info.titulo}</div>
+            <div className="preco">{props.info.preco}</div>
+        </Container>
     )
 }
